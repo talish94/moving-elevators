cddd3589021336f304f569384d550199f66a93e3
Tali <talisch@post.bgu.ac.il>
2021-03-21 02:44:30 +0200

    code design improvement

--- app.js
+++ app.js
@@ -1,91 +1,66 @@
 
 
 let audio = new Audio('/assets/ElevatorBell.mp3');
-let rightElevatorFloor = 2;
-let leftElevatorFloor = 3;
 
-let rightElevatorIdle = false;
-let leftElevatorIdle = false;
-
-let leftDistance = 1000;
-let rightDistance = 1000;
-
-let queueRight = [];
-let queueLeft = [];
-
-let timePerFloor = 4;
-let delayAfterArrival = 2000;
+let timePerFloor = 4;  // in seconds
+let delayAfterArrival = 2000;  // in miliseconds
 
 let firstInitial = true;
 
-let elevatorLeftIsBusy = false;
-let elevatorRightIsBusy = false;
+let arrayOfFloors = [];
+let numOfFloors = 7; // default.
 
-let totalTimeQueueRight = 0;
-let totalTimeQueueLeft = 0;
-
-
-let rightInterval;
-let leftInterval;
-
-let intervalTimerFloor0;
-let intervalTimerFloor1;
-let intervalTimerFloor2;
-let intervalTimerFloor3;
-let intervalTimerFloor4;
-let intervalTimerFloor5;
-let intervalTimerFloor6;
-
-
-let timerFloor0 = false;
-let timerFloor1 = false;
-let timerFloor2 = false;
-let timerFloor3 = false;
-let timerFloor4 = false;
-let timerFloor5 = false;
-let timerFloor6 = false;
-
-
-
+let arrayOfElevators = [];
+let numElevators = 2;
 
 let time_elapsed;
 
- function setAllButtons() {
+initialState();
 
-    console.log(firstInitial);
+function setAllButtons() {
      
-    for (numButtons = 0; numButtons < 7; numButtons++ ){
+    // initialize all floors dictionaries
+    for (floor = 0; floor < numOfFloors; floor++ ){
 
-        let currBTN = "btnFloor" + numButtons;  // "btnFloor0"
-        console.log(currBTN);
-        let elementBTN = document.getElementById(currBTN);
+        arrayOfFloors[floor] = {
+            id: "timerFloor" + floor,
+            isTimerOn: false,
+            interval: null,
+            button: "btnFloor" + floor,
+            redButton: "redBTNFloor" + floor,
+        }; 
 
-        elementBTN.addEventListener("click", onBTNpress.bind(this, numButtons), false);
+        let elementBTN = document.getElementById(arrayOfFloors[floor].button);
+        elementBTN.addEventListener("click", onBTNpress.bind(this, floor), false);
+    }
 
-        console.log(numButtons);
+    // initialize all elevators
+    for (elevator = 0; elevator < numElevators; elevator++ ){
+        arrayOfElevators[elevator] = {
+            id: "elevator" + elevator,
+            queue: [],
+            isBusy: false,
+            totalQueueTime: 0,
+            globalInterval: null,
+            currentFloor: Math.floor(Math.random() * Math.floor(numOfFloors)),   // random initial place of elevator
+        };
+        let currElevator = arrayOfElevators[elevator];
+        document.getElementById(currElevator.id).style.top = convertFloorToPlace(currElevator.currentFloor);
     }
 
     firstInitial = false;
-    console.log(firstInitial);
-
 }
-
-
-
-initialState();
 
 function initialState(){
 
-    if (firstInitial)
+    if (firstInitial){
         setAllButtons();
-
-    if (queueRight.length !== 0 && !elevatorRightIsBusy){
-        moveElevator("elevatorRight", rightElevatorFloor, queueRight[0]);  // num of next floor to go to.
     }
 
-    if (queueLeft.length !== 0 && !elevatorLeftIsBusy) {
-        moveElevator("elevatorLeft", leftElevatorFloor, queueLeft[0]);  // num of next floor to go to.
-
+    for (elevator = 0; elevator < numElevators; elevator++ ){
+        if (arrayOfElevators[elevator].queue.length !== 0 && !arrayOfElevators[elevator].isBusy) {
+            moveElevator(arrayOfElevators[elevator], arrayOfElevators[elevator].currentFloor, arrayOfElevators[elevator].queue[0]); // gets the number of the next floor to go to.
+        }
     }
 
     setTimeout(function() {
@@ -95,32 +70,25 @@ function initialState(){
 
 
 
-function moveElevator(whichElevator, fromFloorNum, toFloorNum){
+function moveElevator(elevator, fromFloorNum, toFloorNum){
 
-    if ( whichElevator == "elevatorRight" )
-        elevatorRightIsBusy = true; 
-    
-    else if (whichElevator == "elevatorLeft")
-        elevatorLeftIsBusy = true;
+    elevator.isBusy = true;
 
-    else
-        throw 'Invalid elevator!';
-
-
-    let topRate = convertFloorToPlace(toFloorNum);
-    document.getElementById(whichElevator).style.top = topRate;
+    let topRate = convertFloorToPlace(toFloorNum);  
+    document.getElementById(elevator.id).style.top = topRate;
 
     let time = (Math.abs(fromFloorNum - toFloorNum)) * timePerFloor;
-    document.getElementById(whichElevator).style.transition = time + "s ease-in-out";
+    document.getElementById(elevator.id).style.transition = time + "s ease-in-out"; // sets duration depends on distance between the 2 floors
 
-    setTimeout(function(){ // free to go again.
-        completeMove(whichElevator, toFloorNum);
-     }, time * 1000); 
+    setTimeout(function(){ 
+        completeMove(elevator, toFloorNum);
+     }, time * 1000);  // only after the elevator gets to its destination
 
      return time;
 }
 
 
+// returns the top % of the current floor
 function convertFloorToPlace(floorNum){  
     
     switch (floorNum) {
@@ -143,296 +111,131 @@ function convertFloorToPlace(floorNum){
     }
 }
 
-
-function completeMove(whichElevator, toFloorNum){
+// removes first element from queue and plays bell ring
+function completeMove(elevator, toFloorNum){
 
     audio.play();
-    let setBusy = "";
-
-    const btnFloor = "redBTNFloor" + toFloorNum;
-    document.getElementById(btnFloor).style.visibility="hidden";
+    document.getElementById(arrayOfFloors[toFloorNum].redButton).style.visibility="hidden";
 
     setTimer(toFloorNum);
-
-
-    if (whichElevator === "elevatorRight" ){
-        setBusy = "right";
-    }
-    
-    else if (whichElevator === "elevatorLeft"){
-        setBusy = "left";
-    }
-
-    else
-        throw 'Invalid elevator!';
-
+    elevator.isBusy = true;
 
     setTimeout(function(){ 
 
-        if (setBusy == "left"){
-            elevatorLeftIsBusy = false; 
-            leftElevatorFloor = queueLeft.shift();
+        elevator.isBusy = false;
+        elevator.currentFloor = elevator.queue.shift();
 
-            if (queueLeft.length === 0 )
-                totalTimeQueueLeft = 0;
-        }
-        else{
-            elevatorRightIsBusy = false; 
-            rightElevatorFloor = queueRight.shift(); 
-            
-
-            if (queueRight.length === 0 )
-                totalTimeQueueRight = 0;
+        if (elevator.queue.length === 0){
+            elevator.totalQueueTime = 0;
         }
 
-    }, delayAfterArrival); 
+    }, delayAfterArrival);  // waits 2 seconds before ready to move again
 }
 
-
-
-
-
+// when calls the elevator - changes the button color and sets the timer
 function onBTNpress(btnFloorNum){
 
-    const redBTNFloor = "redBTNFloor" + btnFloorNum;
-    document.getElementById(redBTNFloor).style.visibility="visible";
+    document.getElementById(arrayOfFloors[btnFloorNum].redButton).style.visibility="visible";
 
-    let right = false;
+    let bestElevator = getBestElevator(btnFloorNum);
+    bestElevator.queue.push(btnFloorNum);
 
-    let destRight = rightElevatorFloor;
-    let destLeft = leftElevatorFloor;
-
-    if (queueRight.length !== 0)
-        destRight = queueRight[queueRight.length-1];
-
-    if (queueLeft.length !== 0)
-        destLeft = queueLeft[queueLeft.length-1];
-    
-
-
-    if ((Math.abs(btnFloorNum - destLeft)) > (Math.abs(btnFloorNum - destRight))){
-        queueRight.push(btnFloorNum);
-        right = true;        
-    }
-        
-    else if ((Math.abs(btnFloorNum - destLeft)) < (Math.abs(btnFloorNum - destRight))){
-        queueLeft.push(btnFloorNum);
-    }
-
-    else {  // distances are equal. choose the first empty one if any
-        if (queueRight.length === 0){
-            queueRight.push(btnFloorNum);
-            right = true;
-        }
-        else {
-            queueLeft.push(btnFloorNum);
-        }
-    }
-
+    calculateElevatorQueueTime(bestElevator, btnFloorNum);
     setTimer(btnFloorNum);
 
-    if ( right ){
-        totalTimeQueueRight = totalTimeQueueRight + (Math.abs(btnFloorNum - destRight) * timePerFloor * 1000);   // global timer !
-    
-        if (queueRight.length > 0 && queueRight[0] !== btnFloorNum){
-            totalTimeQueueRight = totalTimeQueueRight + delayAfterArrival; // delay between floors.
-        }
+    setSpecificFloorTimer(btnFloorNum, bestElevator);
+}
 
-        if (rightInterval == null){
+// when adding a new floor to queue - adds to the totalQueueTime the current duration, and delay if needed (2s)
+function calculateElevatorQueueTime(elevator, btnFloorNum){
 
-            rightInterval = setInterval( function checkTimerRight() {
-                if (queueRight.length !== 0 && totalTimeQueueRight > 100){
-                    totalTimeQueueRight = totalTimeQueueRight - 100;
-                }
-                    
-                else{
-                    clearInterval(rightInterval);
-                    rightInterval = null;
-                }
-            
-            }, 100);
-        }
+    let finalDest = -1; // initialize
 
+    if (elevator.queue.length > 1){
+        finalDest = elevator.queue[elevator.queue.length - 2]
     }
-
     else {
-        totalTimeQueueLeft = totalTimeQueueLeft + (Math.abs(btnFloorNum - destLeft) * timePerFloor * 1000); 
+        finalDest = elevator.currentFloor;
+    }
 
-        if (queueLeft.length > 0 && queueLeft[0] !== btnFloorNum)
-            totalTimeQueueLeft = totalTimeQueueLeft + delayAfterArrival; // delay between floors.
-        
-        if (leftInterval == null){
+    elevator.totalQueueTime += (Math.abs(btnFloorNum - finalDest) * timePerFloor * 1000);   // global timer !
+    
+    if (elevator.queue.length > 0 && elevator.queue[0] !== btnFloorNum){
+        elevator.totalQueueTime += delayAfterArrival; // delay between floors.
+    }
 
-            leftInterval = setInterval( function checkTimerRight() {
+    if (elevator.interval == null){
 
-                if (queueLeft.length !== 0 && totalTimeQueueLeft > 100)
-                    totalTimeQueueLeft = totalTimeQueueLeft - 100;
+        // sets global interval for current elevator's queue
+        elevator.interval = setInterval( function checkTimerRight() {
+            if (elevator.queue.length !== 0 && elevator.totalQueueTime > 100){
+                elevator.totalQueueTime -= 100;
+            }
                 
-                else{
-                    clearInterval(leftInterval);
-                    leftInterval = null;
-                }
-            
-            }, 100);
-        }
-    }
-
-    let whichQueue = right ? totalTimeQueueRight : totalTimeQueueLeft;
-    setSpecificFloorTimer(btnFloorNum, whichQueue);
-}
-
-function setSpecificFloorTimer(floorNum, whichQueue){   // set timer on or off, depends on 'boolean'.
-
-    let currTimer = "timerFloor" + floorNum;
-
-    switch (currTimer) {
-        case "timerFloor0": {
-            timerFloor0 = true;
-            let currTime0 = whichQueue / 1000 ;
-
-            intervalTimerFloor0 = setInterval( function checkTimer0() {
-                if (currTime0 > 0){
-                    currTime0 = currTime0 - 0.1;
-                    currTime0 = currTime0.toFixed(1);
-                    document.getElementById('timerFloor0').innerText = currTime0;
-                }
-
-                else{
-                    clearInterval(intervalTimerFloor0);
-                }
-            
-            }, 100);
-            return;
-        }
-
-        case "timerFloor1":{
-            timerFloor1 = true;
-            let currTime1 = whichQueue / 1000 ;
-
-            intervalTimerFloor1 = setInterval( function checkTimer1() {
-                if (currTime1 > 0.09){
-                    currTime1 = currTime1 - 0.1;
-                    currTime1 = currTime1.toFixed(1);
-                    document.getElementById('timerFloor1').innerText = currTime1;
-                }
-
-                else{
-                    clearInterval(intervalTimerFloor1);
-                }
-            
-            }, 100);
-            return;
-        }
-
-        case "timerFloor2": {
-            timerFloor2 = true;
-            let currTime2 = whichQueue / 1000 ;
-
-            intervalTimerFloor2 = setInterval( function checkTimer2() {
-                if (currTime2 > 0.09){
-                    currTime2 = currTime2 - 0.1;
-                    currTime2 = currTime2.toFixed(1);
-                    document.getElementById('timerFloor2').innerText = currTime2;
-                }
-
-                else{
-                    clearInterval(intervalTimerFloor2);
-                }
-            
-            }, 100);
-
-            return;
-        }
-
-        case "timerFloor3": {
-            timerFloor3 = true;
-            let currTime3 = whichQueue / 1000 ;
-
-            intervalTimerFloor3 = setInterval( function checkTimer3() {
-                if (currTime3 > 0.09){
-                    currTime3 = currTime3 - 0.1;
-                    currTime3 = currTime3.toFixed(1);
-                    document.getElementById('timerFloor3').innerText = currTime3;
-                }
-
-                else{
-                    clearInterval(intervalTimerFloor3);
-                }
-            
-            }, 100);
-            return;
-        }
-
-        case "timerFloor4":{
-            timerFloor4 = true;
-            let currTime4 = whichQueue / 1000 ;
-
-            intervalTimerFloor4 = setInterval( function checkTimer4() {
-                if (currTime4 > 0.09){
-                    currTime4 = currTime4 - 0.1;
-                    currTime4 = currTime4.toFixed(1);
-                    document.getElementById('timerFloor4').innerText = currTime4;
-                }
-
-                else{
-                    clearInterval(intervalTimerFloor4);
-                }
-            
-            }, 100);
-            return;
-        }
-    
-        case "timerFloor5":{
-            timerFloor5 = true;
-            let currTime5 = whichQueue / 1000 ;
-
-            intervalTimerFloor5 = setInterval( function checkTimer5() {
-                if (currTime5 > 0.09){
-                    currTime5 = currTime5 - 0.1;
-                    currTime5 = currTime5.toFixed(1);
-                    document.getElementById('timerFloor5').innerText = currTime5;
-                }
-
-                else{
-                    clearInterval(intervalTimerFloor5);
-                }
-            
-            }, 100);            
-            return;
-        }
-
-        case "timerFloor6":{
-            timerFloor6 = true;
-            let currTime6 = whichQueue / 1000 ;
-
-            intervalTimerFloor6 = setInterval( function checkTimer6() {
-                if (currTime6 > 0.09){
-                    currTime6 = currTime6 - 0.1;
-                    currTime6 = currTime6.toFixed(1);
-                    document.getElementById('timerFloor6').innerText = currTime6;
-                }
-
-                else{
-                    clearInterval(intervalTimerFloor6);
-                }
-            
-            }, 100);
-            return;
-        }
-    
-        default:
-            console.log("Invalid timer!");
+            else{
+                clearInterval(elevator.interval);
+                elevator.interval = null;
+            }
+        
+        }, 100); // implements a countdown timer
     }
 }
 
+// return the closest elevator to the specific requested btnFloorNum (distance, not time)
+function getBestElevator(btnFloorNum){
+    let minDistance = numOfFloors;
+    let bestElevator = null;
+
+    for (elevator = 0; elevator < numElevators; elevator++ ){
+
+        let currElevator = arrayOfElevators[elevator];
+        let finalDest = currElevator.queue.length !== 0 ? currElevator.queue[currElevator.queue.length - 1] : currElevator.currentFloor;
+        let dist = Math.abs(btnFloorNum - finalDest);
+
+        // checks if min value needs to be updated
+        if (dist < minDistance) {
+            minDistance = dist;
+            bestElevator = currElevator;
+        }
+    }
+    return bestElevator;
+}
+
+// sets countdown timer per requested floor
+function setSpecificFloorTimer(requestedFloor, elevator){   
+
+    let currFloor = arrayOfFloors[requestedFloor];
+    let elevatorQueueTime = elevator.totalQueueTime;
+    let currTime = elevatorQueueTime / 1000 ;
+
+    currFloor.interval = setInterval( function checkTimer() {
+
+        if (currTime > 0){
+
+            console.log(currTime);
+            currTime = currTime - 0.1;
+            currTime = currTime.toFixed(1);
+            document.getElementById(currFloor.id).innerText = currTime;
+        }
+
+        else{
+            clearInterval(currFloor.interval);
+        }
+    
+    }, 100);
+}
+
+// hides or displays the current timer div
 function setTimer(btnFloorNum){
 
-    const timerFloor = "timerFloor" + btnFloorNum;
+    let currentTimer = arrayOfFloors[btnFloorNum].id;
 
-    if (document.getElementById(timerFloor).style.display === "block")
-        document.getElementById(timerFloor).style.display = "none";
+    if (document.getElementById(currentTimer).style.display === "block"){
+        document.getElementById(currentTimer).style.display = "none";
+    }
 
-    else
-        document.getElementById(timerFloor).style.display = "block";
+    else{
+        document.getElementById(currentTimer).style.display = "block";
+    }
 
 }
